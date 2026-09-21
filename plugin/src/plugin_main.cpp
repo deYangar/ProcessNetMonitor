9e938f0e73f4674a71dce3b6ddf118e360b2662a
deYangar <70438628+deYangar@users.noreply.github.com>
2026-09-21 17:08:53 +0800

    docs: v1.16.4 release note + CHANGELOG + 版本号 bump

--- plugin/src/plugin_main.cpp
+++ plugin/src/plugin_main.cpp
@@ -884,7 +884,7 @@ const wchar_t* CProcessNetPlugin::GetInfo(PluginInfoIndex i) {
     case TMI_DESCRIPTION: return L"Per-process network speed";
     case TMI_AUTHOR: return L"deYangar";
     case TMI_COPYRIGHT: return L"MIT";
-    case TMI_VERSION: return L"1.16.3";
+    case TMI_VERSION: return L"1.16.4";
     case TMI_URL: return L"https://github.com/deYangar/ProcessNetMonitor";
     default: return L"";
     }
